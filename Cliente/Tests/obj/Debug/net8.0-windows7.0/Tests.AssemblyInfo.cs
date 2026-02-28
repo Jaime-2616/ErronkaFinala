c44b838cd2a6ac6761e8c36753056ab25bfec8fe
Jaime-2616 <jmaneromo25paag@ikzubirimanteo.com>
2026-02-28 22:30:34 +0100

    Prueba

--- Cliente/Tests/obj/Debug/net8.0-windows7.0/Tests.AssemblyInfo.cs
+++ Cliente/Tests/obj/Debug/net8.0-windows7.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63edbd82f0e56ded65de8eda7b8a32507d1996e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
